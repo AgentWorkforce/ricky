0db8a9c2fdf8e93ffe674f8dbc0164a350426363
Khaliq <khaliqgant@gmail.com>
2026-04-27 22:08:10 -0700

    package publish

---
+++ src/bin/ricky.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { cliMain } from '../surfaces/cli/commands/cli-main.js';
+
+const result = await cliMain();
+
+if (result.output.length > 0) {
+  process.stdout.write(`${result.output.join('\n')}\n`);
+}
+
+process.exitCode = result.exitCode;
